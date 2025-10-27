23c4a8841ff1425290083551d3ca961e8dd3e23d
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-10-27 19:27:35 +0000

    Adds Biome and Lefthook for pre-commit checks

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,55 +1,56 @@
 /// <reference types="vite/client" />
-import type { ReactNode } from 'react'
+
 import {
-  Outlet,
-  createRootRoute,
-  HeadContent,
-  Scripts,
-} from '@tanstack/react-router'
-import styles from '../styles.css?url'
+	createRootRoute,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from "@tanstack/react-router";
+import type { ReactNode } from "react";
+import styles from "../styles.css?url";
 
 export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'Propellant',
-      },
-    ],
-    links: [
-      {
-        rel: "stylesheet",
-        href: styles
-      }
-    ]
-  }),
-  component: RootComponent,
-})
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "Propellant",
+			},
+		],
+		links: [
+			{
+				rel: "stylesheet",
+				href: styles,
+			},
+		],
+	}),
+	component: RootComponent,
+});
 
 function RootComponent() {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  )
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	);
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
-  return (
-    <html>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<Scripts />
+			</body>
+		</html>
+	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [tsConfigPaths(), tanstackStart(), viteReact(), tailwindcss()],
+	server: {
+		port: 3000,
+	},
+	plugins: [tsConfigPaths(), tanstackStart(), viteReact(), tailwindcss()],
 });
