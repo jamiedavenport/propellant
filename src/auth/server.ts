eeecc5e5387dea7b014f8ae4cd8d926c055425c2
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-10-30 16:45:23 +0000

    Enable session cookie cache

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -9,4 +9,10 @@ export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "pg",
 	}),
+	session: {
+		cookieCache: {
+			enabled: true,
+			maxAge: 5 * 60, // 5s
+		},
+	},
 });
