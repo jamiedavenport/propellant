cc79386e4904c9c88616cca624beefc6b53755ff
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-11-04 12:06:22 +0000

    Add header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,37 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { FireIcon } from "@phosphor-icons/react";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { Button } from "~/components/ui/button";
 
 export const Route = createFileRoute("/")({
 	component: RouteComponent,
+	loader: async ({ context }) => {
+		return {
+			authenticated: !!context.auth,
+		};
+	},
 });
 
 function RouteComponent() {
-	return <div>Hello "/"!</div>;
+	const { authenticated } = Route.useLoaderData();
+
+	return (
+		<div>
+			<header className="flex items-center justify-between p-10">
+				<h1 className="flex items-center gap-1">
+					<FireIcon weight="duotone" className="size-8" />
+					<span className="text-lg font-bold hidden md:block">Propellant</span>
+				</h1>
+
+				{authenticated ? (
+					<Button asChild>
+						<Link to="/inbox">Dashboard</Link>
+					</Button>
+				) : (
+					<Button asChild>
+						<Link to="/sign/in">Sign In</Link>
+					</Button>
+				)}
+			</header>
+		</div>
+	);
 }
